290bb36c4d165cd2039b33191ff9142a89859703
Andy Alexander <andy@balloonindustries.com>
2017-12-11 02:13:44 -0500

    Bug fixes

--- content.js
+++ content.js
@@ -1,32 +1,32 @@
 /*
-        Chinese Personalized Colors - A tone colorizer
-        Copyright (C) 2017 Andrew Alexander
-        ---
-
-        Originally based on Synesthete
-        Copyright (C) 2015 Adam Savvas
-        https://chrome.google.com/webstore/detail/synesthetize/ldljgghnflfphlnpneghciodeehilana
-
-        ---
-
-        This program is free software; you can redistribute it and/or modify
-        it under the terms of the GNU General Public License as published by
-        the Free Software Foundation; either version 2 of the License, or
-        (at your option) any later version.
-
-        This program is distributed in the hope that it will be useful,
-        but WITHOUT ANY WARRANTY; without even the implied warranty of
-        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-        GNU General Public License for more details.
-
-        You should have received a copy of the GNU General Public License
-        along with this program; if not, write to the Free Software
-        Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-
-        ---
-
-        Please do not change or remove any of the copyrights or links to web pages
-        when modifying any of the files.
+ Chinese Personalized Colors - A tone colorizer
+ Copyright (C) 2017 Andrew Alexander
+ ---
+ 
+ Originally based on Synesthete
+ Copyright (C) 2015 Adam Savvas
+ https://chrome.google.com/webstore/detail/synesthetize/ldljgghnflfphlnpneghciodeehilana
+ 
+ ---
+ 
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+ 
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ 
+ ---
+ 
+ Please do not change or remove any of the copyrights or links to web pages
+ when modifying any of the files.
  */
 
 
@@ -41,15 +41,17 @@
                 "text": char
             },
                     function (char_return) {
-                        var regex = '\\[\\w+([0-9]+)\\]';
-                        var tone = char_return.data[0][0].match(regex);
-                        
-                        if (typeof tone[1] !== null) {
-                            chrome.storage.sync.get("tone_" + tone[1], function (obj) {
-                                var tone_color = Object.values(obj)[0];
-                                $('body').highlight(char_return.data[0][1], 'tone-' + tone[1]);
-                                $("<style type='text/css'> ." + "tone-" + tone[1] + "{ color:" + tone_color + "} </style>").appendTo("head");
-                            });
+                        if (typeof char_return === 'undefined') {
+                            var regex = '\\[\\w+([0-9]+)\\]';
+                            var tone = char_return.data[0][0].match(regex);
+
+                            if (typeof tone[1] === 'undefined') {
+                                chrome.storage.sync.get("tone_" + tone[1], function (obj) {
+                                    var tone_color = Object.values(obj)[0];
+                                    $('body').highlight(char_return.data[0][1], 'tone-' + tone[1]);
+                                    $("<style type='text/css'> ." + "tone-" + tone[1] + "{ color:" + tone_color + "} </style>").appendTo("head");
+                                });
+                            }
                         }
                     });
 
